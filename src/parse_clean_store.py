be24b6a697e46a9de38b23d1d7725ae881a2b01f
davidblaszka <djblazz@gmail.com>
2017-03-15 12:34:30 -0700

    updates

--- src/parse_clean_store.py
+++ src/parse_clean_store.py
@@ -4,6 +4,7 @@ import time
 from urllib import urlencode
 import selenium.webdriver
 import random
+import pandas as pd
 
 def parse_route_page(html):
     '''
@@ -21,12 +22,9 @@ def parse_route_page(html):
     
     for i, td in enumerate(page_tag.find('table').findAll('td')): 
         if td.text.split(':')[0] == 'Type':
-            route_type = clean_route_type(page_tag.find('table').findAll('td')[i+1])
-            route_dict['route_type_list'] = route_type[0]
-            route_dict['pitches'] = route_type[1]
-            route_dict['height'] = route_type[2]
-            route_dict['uiaa'] = route_type[3]
-            route_dict['misc'] = route_type[4]
+            route_type_dict = clean_route_type(page_tag.find('table').findAll('td')[i+1])
+            for key, value in zip(route_type_dict.keys(), route_type_dict.values()):
+            	route_dict[key] = value
         elif td.text.split(':')[0] == 'Original':
             o_grade = clean_original_grade(page_tag.find('table').findAll('td')[i+1])
             route_dict['original_grade'] = o_grade
@@ -61,49 +59,47 @@ def clean_first_ascent(tag):
 			fa_date = item
 	return fa_people, fa_date
 
+
 def clean_route_type(tag):
 	route_type = tag.text
 	route_type = route_type.encode('utf-8').split(', ')
-	list_of_types = ['Trad', 'Sport', 'TR', 'Alpine', 
-					'Ice', 'Boulder', 'Aid', 'Mixed']
 	# initialize variables
-	route_type_list = []
-	pitches = float('nan')
-	height = float('nan')
-	uiaa = float('nan')
-	misc = float('nan')
+	route_type_dict = {'Trad': 0, 'Sport': 0, 'TR': 0, 'Alpine': 0, 
+                'Ice': 0, 'Boulder': 0, 'Aid': 0, 'Mixed': 0, 
+                   'pitches': float('nan'),'uiaa':float('nan'), 
+                   'height': float('nan')}
+	list_of_types = ['Trad', 'Sport', 'TR', 'Alpine', 
+	                'Ice', 'Boulder', 'Aid', 'Mixed']
 	for item in route_type:
-		if item in list_of_types:
-			route_type_list.append(item)
-		elif 'pitches' in item:
-			pitches = item.replace(' pitches', '')
-		elif 'pitch' in item:
-			pitches = item.replace(' pitch','')
-		elif "'" in item:
-			height = float(item.replace("'",''))
-		elif 'Grade' in item:
-			uiaa = item
-		else:
-			misc = item
-
-	return route_type_list, pitches, height, uiaa, misc
+	    for item2 in route_type_dict.keys():
+	        if item2 in item and item2 in list_of_types:
+	            route_type_dict[item2] = 1 
+	        elif 'pitches' in item:
+	            route_type_dict['pitches'] = int(item.replace(' pitches', ''))
+	        elif 'pitch' in item:
+	            route_type_dict['pitches'] = int(item.replace(' pitch',''))
+	        elif "'" in item:
+	            route_type_dict['height'] = float(item.replace("'",''))
+	        elif 'Grade' in item:
+	            route_type_dict['uiaa'] = item
+	return route_type_dict
 
 
 def clean_submitted_by(tag):
-	sb = page_tag.text
+	sb = tag.text
 	sb = sb.encode('utf-8').split(' on ')
 	return sb[0], sb[1]
 
 
 def clean_original_grade(tag):
-	grade = page_tag.text
+	grade = tag.text
 	grade = grade.encode('utf-8').split('\xa0')[1]
 	grade = grade.split('French')[0]
 	return grade
 
 
 def clean_grade(tag):
-	grade = page_tag.find('span', {'class':'rateYDS'}).text
+	grade = tag.find('span', {'class':'rateYDS'}).text
 	grade = grade.encode('utf-8').split('\xa0')[1]
 	return grade
 
@@ -121,33 +117,67 @@ def clean_average_rating(soup):
 
 
 def parse_user(html):
-    '''returns user info'''
-    # make user dict
-    soup = BeautifulSoup(html, 'html.parser')
-    user_dict = {}
-    user_dict['name'] = str(soup.find('h1').text)
-    side_bar = soup.select('div.roundedBottom')[0].text.split('\n') # side bar
-    user_dict['member_since'] = side_bar[3].split('Since: ')[1]
-    user_dict['last_vist'] = side_bar[4].split('Visit: ')[1]
-    user_dict['point_rank'] = side_bar[9].split('Rank: # ')[1]
-    user_dict['total_points'] = side_bar[10].split('Points: ')[1].replace(',', '')
-    user_dict['compliments'] = side_bar[11].split(' Compliments')[0]
-    
-    for item in soup.find('div',{'class': 'personalData'}).text.split('\n'):
-        if 'Personal' in item:
-            user_dict['personal'] = item.split('Personal: ')[1]
-        elif 'Favorite Climbs:' in item:
-            user_dict['favorite_climbs'] = item.split('Favorite Climbs: ')[1]
-        elif 'Other Interests:' in item:
-            user_dict['other_interests'] = item.split('Other Interests: ')[1]
-        elif 'Likes to climb:' in item:
-            user_dict['likes_to_climb'] = item.split('Likes to climb: ')[1]
-        elif 'Trad:' in item:
-            user_dict['trad'] = item.split('Trad:')[1]
-        elif 'Sport:' in item:
-            user_dict['sport'] = item.split('Sport:')[1]
-        elif 'Aid:' in item:
-            user_dict['aid'] = item.split('Aid:')[1]
-        elif 'Ice:' in item:
-            user_dict['ice'] = item.split('Ice:')[1]
-    return user_dict
+	'''returns user info'''
+	# make user dict
+	soup = BeautifulSoup(html, 'html.parser')
+
+	user_dict = {'Personal:': float('nan'),
+					'Favorite Climbs:': float('nan'),
+					'Other Interests:': float('nan'),
+					'Likes to climb:': float('nan'),
+					'Trad:': float('nan'),
+					'Sport:': float('nan'),
+					'Aid:': float('nan'),
+					'Ice:': float('nan')}
+
+	for item in soup.find('div',{'class': 'personalData'}).text.split('\n'):
+		for item2 in user_dict.keys():
+			if item2 in item:
+				item_list = item.split(item2)
+				if len(item_list) > 1:
+					user_dict[item2] = item_list[1]
+
+	user_dict['name'] = soup.find('h1').text.encode('utf-8')
+
+	side_bar = soup.select('div.roundedBottom')[0].text.split('\n') # side bar
+	user_list = ['Since: ', 'Visit: ', 'Rank: # ', 'Points: ', ' Compliments']
+	labels = ['member_since', 'last_vist', 'point_rank', 'total_points']
+	for item1 in side_bar:
+		for item2, key in zip(user_list, labels):
+			if item2 in item1:
+				item_list = item1.split(item2)
+				if len(item_list) > 1:
+					user_dict[key] = item_list[1]
+	'''
+	member_since = side_bar[3].split('Since: ')
+	if len(member_since) > 1:
+		user_dict['member_since'] = member_since[1]
+	else:
+		member_since = float('nan')
+	user_dict['last_vist'] = side_bar[4].split('Visit: ')[1]
+	user_dict['point_rank'] = side_bar[9].split('Rank: # ')[1]
+	user_dict['total_points'] = side_bar[10].split('Points: ')[1].replace(',', '')
+	'''
+	user_dict['compliments'] = side_bar[11].split(' Compliments')[0]
+	return user_dict
+
+
+def clean_ratings(rating_by_user_df):
+	'''takes a dataframe'''
+
+	# make empty dict to fill
+	rating_dict = {}
+	user_dict = []
+	routes = []
+	# convert route name to utf-8
+	for route, usernames, ratings in zip(rating_by_user_df['route'], 
+										rating_by_user_df['username'],
+										rating_by_user_df['rating']):
+		routes.append(route.encode('utf-8'))
+		d = {}
+		for username, rating in zip(usernames, ratings):
+			d[username] = rating
+			user_dict.append(d)
+
+	return pd.DataFrame(user_dict, index=routes)
+

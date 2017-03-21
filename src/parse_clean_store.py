96265664ec9d363824299260e160512dcd4b873a
davidblaszka <djblazz@gmail.com>
2017-03-20 19:55:14 -0700

    redo scrape

--- src/parse_clean_store.py
+++ src/parse_clean_store.py
@@ -49,6 +49,7 @@ def parse_route_page(_id, html):
 
 
 def clean_first_ascent(tag):
+	'''cleans and sorts FA'''
 	fa = tag.text.encode('utf-8')
 	fa_list = fa.split(',')
 	fa_people = []
@@ -119,11 +120,10 @@ def clean_average_rating(soup):
 	return average_rating
 
 
-def parse_user(html):
+def parse_user(html, _id):
 	'''returns user info'''
-	# make user dict
 	soup = BeautifulSoup(html, 'html.parser')
-
+	# make user dict
 	user_dict = {'Personal:': np.nan,
 					'Favorite Climbs:': np.nan,
 					'Other Interests:': np.nan,
@@ -132,7 +132,9 @@ def parse_user(html):
 					'Sport:': np.nan,
 					'Aid:': np.nan,
 					'Ice:': np.nan}
-
+	
+	# make id
+	user_dict['id'] = _id
 	for item in soup.find('div',{'class': 'personalData'}).text.split('\n'):
 		for item2 in user_dict.keys():
 			if item2 in item:
@@ -154,6 +156,144 @@ def parse_user(html):
 
 	user_dict['compliments'] = side_bar[11].split(' Compliments')[0]
 	return user_dict
+
+
+def parse_clean_user(html, _id):
+	'''returns user info'''
+	soup = BeautifulSoup(html, 'html.parser')
+	# make user dict
+	user_dict = {'Favorite Climbs:': np.nan,
+					'Other Interests:': np.nan}
+
+	for item in soup.find('div',{'class': 'personalData'}).text.split('\n'):
+		for item2 in user_dict.keys():
+			if item2 in item:
+				item_list = item.split(item2)
+				if len(item_list) > 1:
+					user_dict[item2] = item_list[1]
+		user_dict = check_climb_types(item, user_dict)
+		user_dict = check_personal(item, user_dict)
+		user_dict = clean_likes_to_climb(item, user_dict)
+	# make id
+	user_dict['id'] = _id
+	# get username
+	user_dict['name'] = soup.find('h1').text.encode('utf-8')
+
+	side_bar = soup.select('div.roundedBottom')[0].text.split('\n') # side bar
+	user_dict = clean_points_visits(side_bar, user_dict)
+	user_dict =  clean_compliments(side_bar[11], user_dict)
+	return user_dict
+
+def clean_points_visits(tag, user_dict):
+	'''cleans left bar'''
+	user_list = ['Since: ', 'Visit: ', 'Rank: # ', 'Points: ', ' Compliments']
+	labels = ['member_since', 'last_vist', 'point_rank', 'total_points']
+	for item1 in tag:
+		for item2, key in zip(user_list, labels):
+			if item2 in item1:
+				item_list = item1.split(item2)
+				if len(item_list) > 1:
+					if key in ['point_rank', 'total_points']:
+						points = item_list[1].replace(',', '')
+						if points == '-none-':
+							user_dict[key] = np.nan
+						else:
+							user_dict[key] = int(points)
+					else:	
+						user_dict[key] = item_list[1]
+	return user_dict
+
+def clean_compliments(tag, user_dict):
+	'''turns compliments into ints'''
+	compliments = tag.split(' Compliments')[0]
+	compliments = compliments.replace(',' , '')
+	if compliments == '':
+		compliments = 0
+	user_dict['compliments'] = int(compliments)
+	return user_dict
+
+
+def clean_likes_to_climb(item, user_dict):
+	'''finds what climbing styles the user likes'''
+	climb_list = ['Trad', 'Sport', 'TR', 'Gym']
+	key_list = ['likes_trad', 'likes_sport', 'likes_tr', 'likes_gym']
+	item2 = 'Likes to climb:'
+	if item2 in item:
+		item_list = item.split(item2)
+		for key, item2 in zip(key_list, climb_list):
+			if len(item_list) > 1:
+					if item2 in item_list[1]:
+						user_dict[key] = 1
+					else:
+						user_dict[key] = 0
+			else:
+				user_dict[key] = 0
+	return user_dict
+
+
+def check_personal(item, user_dict):
+	'''get city, state, age, and sex'''
+	if 'Personal:' in item:
+		item_list = item.split('Personal:')
+		if len(item_list) > 1:
+			if 'Lives in' in item_list[1]:
+				personal_list = item_list[1].replace('Lives in', '').split(',')
+				city = personal_list[0].encode('utf-8')
+				user_dict['city'] = city
+				if len(personal_list) > 1:
+					state = personal_list[1].encode('utf-8')
+					user_dict['state'] = state
+				if 'years old' in item_list[1]:
+					age = item_list[1].split(' years old')[0]
+					age = int(age[::-1][0:2])
+					user_dict['age'] = age
+				if 'Female' in item_list[1]:
+					user_dict['Female'] = 1
+				else:
+					user_dict['Female'] = 0
+		else:
+			user_dict['city'] = np.nan
+			user_dict['state'] = np.nan
+			user_dict['age'] = np.nan
+			user_dict['Female'] = np.nan
+
+	return user_dict
+
+
+def check_climb_types(item, user_dict):
+	'''fills user_dict with climbing types'''
+	climb_type_list = ['Trad:', 'Sport:', 'Aid:', 'Ice:']
+	key_list = [('trad_lead', 'trad_follows'),
+				('sport_lead', 'sport_follows'), 
+				('aid_leads', 'aid_follows'), 
+				('ice_leads','ice_follows')]
+	for key, item2 in zip(key_list, climb_type_list):
+			if item2 in item:
+				item_list = item.split(item2)
+				if len(item_list) > 1:
+					leads, follows = clean_climb_types(item_list[1])
+					user_dict[key[0]] = leads
+					user_dict[key[1]] = follows
+				else:
+					user_dict[key[0]] = np.nan
+					user_dict[key[1]] = np.nan
+	return user_dict
+
+
+def clean_climb_types(s):
+	'''cleans climbing type strings'''
+	s_list = s.split("Follows")
+	leads_list = s_list[0].encode('utf-8').split('Leads ') 
+	if len(leads_list) < 2:
+		leads = np.nan
+		follows = np.nan
+	else:
+		leads = leads_list[1].split('\xc2')[0][-3:]
+		if len(s_list) > 1:
+			follows = s_list[1].encode('utf-8')[-3:]
+		else:
+			follows_list = np.nan
+	return leads, follows
 
 
 def create_ratings_matrix(df):
--- src/scraper_main.py
+++ src/scraper_main.py
@@ -1,27 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
-import time
-from urllib import urlencode
 import selenium.webdriver
-import random
-from pymongo import MongoClient
-import json
-from route_url_scraper import search_route_page
-from route_url_scraper import all_route_urls 
-from route_url_scraper import scrape_route_page
-from route_url_scraper import scrape_ratings_by_user
-from route_url_scraper import scrape_user
-from store_in_database import add_to_route_html_database
-from store_in_database import add_to_rating_database
-from store_in_database import add_to_user_html_database
-from store_in_database import add_route_link_database
+from route_url_scraper import (search_route_page, 
+								all_route_urls, 
+								scrape_route_page, 
+								scrape_ratings_by_user,
+								scrape_user)
+from store_in_database import (add_to_route_html_database, 
+								add_to_rating_database, 
+								add_to_user_html_database, 
+								add_route_link_database)
 
 
 if __name__ == "__main__":
 	# define grade search tuples 
     grades = [(800,1900), (2000,2500), 
-             (2600,3100), (3100,3500), (4600,5500), 
-             (6600,12400)]
+             (2600,3100), (3100,3500), 
+             (4600,5500), (6600,12400)]
     # make empty list to fill with route page urls
     route_href_list = [] 
     for grade in grades:
@@ -35,6 +28,7 @@ if __name__ == "__main__":
 	for route_href in route_href_list:
 	    star_url, route_html, route_name = scrape_route_page(route_href, browser)
 	    route_html_dict = {route_name_utf8: route_html}
+	    route_html_dict['user_url'] = star_url
 	    add_to_route_html_database(route_html_dict)
 	    user_url, rating_dict = scrape_ratings_by_user(star_url, browser)
 	    rating_dict['route'] = route_name
